a368b46ab5ec5e46d5c3d66e2b0fd837905a0d2f
David <20544dk@gmail.com>
2019-12-18 09:07:20 +1000

    wednesday-start

---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Menu = ()=>{
+    return(
+        <div className="menu">
+            <div className="flexC">
+                <div className="h-1">Bible Quiz</div>
+                <div className='sub-h1'>A simple quiz about the books of the bible</div>
+                <div className='btn btn-filled'>Start</div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Menu
